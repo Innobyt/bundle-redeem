33878c6857b297265f76c400e9cbba4d7bcf03fa
Kyle <kyle@innobyt.com>
2014-10-30 10:48:17 +0800

    project files

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/redemption-dev'
+  },
+
+  seedDB: true
+};
